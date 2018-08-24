702eec31d822b15c2eea623484c7c9f46cdf37ff
vighnesh <vighneshaka8921@gmail.com>
2018-08-25 00:52:25 +0530

    api calls are working, images are showing, gotta get some sleep now

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
-import {api} from './src/MainScreen.js'
+import MainScreen from "./src/MainScreen.js"
 
 export default class App extends Component{
   render() {
     return (
-      <View>
-        <Text>{api}</Text>
-      </View>
+      <MainScreen />
     );
   }
 }
--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,2 +1,31 @@
-const api = "36573976f10a519ed2b142e6e9685001";
-export {api};
+import React, {Component } from 'react';
+import {
+    View,
+    Text,
+    Image
+} from 'react-native';
+import {api} from './api.js';// get your api key from   https://openweathermap.org
+import axios from 'axios';
+
+class MainScreen extends Component{
+    componentDidMount() {
+        axios.get('https://samples.openweathermap.org/data/2.5/weather?q=London,uk&appid='+{api})//api key is hidden
+          .then(res => {
+            console.log(res.data);
+            alert(res.data.coord.lon)
+          })
+      }
+    
+    render(){
+        return(
+            <View style = {{height:100+'%',width:100+'%'}}>
+            <Image style = {{flex:1}}source = {{uri: 'https://images.pexels.com/photos/1182702/pexels-photo-1182702.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940'}}/>
+            </View>    
+    )
+    }
+}
+
+const styles  = {
+    
+}
+export default MainScreen;
